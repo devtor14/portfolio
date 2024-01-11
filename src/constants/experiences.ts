b9bb3bf5fcc2e4f2cdf24eaa3d89cb76756e4bbe
Hector14oo <devtor14@gmail.com>
2024-01-11 17:03:18 -0500

    :arrow_right: Add constants foolder, projects and experiences

---
+++ src/constants/experiences.ts
@@ -0,0 +1,15 @@
+const experience1 = {
+  title: 'Freelancer',
+  date: '2022-Present',
+  body: 'Design and develop web applications using technologies such as JavaScript, React, Tailwind, Astro, etc. allowing me to deliver high-quality products that are user-friendly, visually appealing, and perform exceptionally well.',
+  bg: 'bg-Pink',
+};
+const experience2 = {
+  title: 'Frontend Developer',
+  at: 'Inktradex',
+  date: '2022',
+  body: 'Inktradex is a translation platform for various types of documents in multiple languages. The goal was to enhance the user experience by making UI changes, adding animations, and improving the content.',
+  bg: 'bg-Mustard',
+};
+
+export const experiences = [experience1, experience2]
---
+++ src/constants/projects.ts
@@ -0,0 +1,26 @@
+const project1 = {
+  title: 'Andromedia • REST API',
+  body: 'Retrieve information about movies, TV shows, and personalities from the entertainment world.',
+  stack: 'Java-Script, Nodejs, Express.js, MongoDB',
+  svgPath: './src/assets/Andromedia.svg' 
+};
+const project2 = {
+  title: 'Form Multi Steps • Web',
+  body: 'A multi-step form that simulates the service payment process.',
+  stack: 'TypeScript, React, Next.js 13, Tailwind-CSS.',
+  svgPath: './src/assets/Form.svg' 
+};
+const project3 = {
+  title: 'Movies APP • Web APP',
+  body: 'An interactive website designed for movie information, weekly trending insights and trailers.',
+  stack: 'TypeScript, React, Next.js 13, Css Modules, Firebase, Figma.',
+  svgPath: './src/assets/Cinema.svg' 
+};
+const project4 = {
+  title: 'Inktradex • Landing Page',
+  body: 'Inktradex offers 360-degree translation solutions for companies of all sizes and sectors.',
+  stack: 'Type-Script, Nextjs, Reactjs, styled-components, ',
+  svgPath: './src/assets/Inktradex.svg' 
+};
+
+export const projects = [project1, project2, project3, project4];
