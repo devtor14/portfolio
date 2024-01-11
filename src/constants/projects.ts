7068918ac0e378d4628aaf391529a1d8ae9fe952
Hector14oo <devtor14@gmail.com>
2024-01-11 18:19:56 -0500

    :test_tube: Testing img path on the deploy

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,7 +4,7 @@ const project1 = {
   title: 'Andromedia • REST API',
   body: 'Retrieve information about movies, TV shows, and personalities from the entertainment world.',
   stack: 'Java-Script, Nodejs, Express.js, MongoDB',
-  svgPath: './src/assets/Andromedia.svg' 
+  svgPath: '/assets/Andromedia.svg' 
 };
 const project2 = {
   status: 'REPOSITORY',
@@ -12,7 +12,7 @@ const project2 = {
   title: 'Form Multi Steps • Web',
   body: 'A multi-step form that simulates the service payment process.',
   stack: 'TypeScript, React, Next.js 13, Tailwind-CSS.',
-  svgPath: './src/assets/Form.svg' 
+  svgPath: '/assets/Form.svg' 
 };
 const project3 = {
   status: 'REPOSITORY',
@@ -20,7 +20,7 @@ const project3 = {
   title: 'Movies APP • Web APP',
   body: 'An interactive website designed for movie information, weekly trending insights and trailers.',
   stack: 'TypeScript, React, Next.js 13, Css Modules, Firebase, Figma.',
-  svgPath: './src/assets/Cinema.svg' 
+  svgPath: '/assets/Cinema.svg' 
 };
 const project4 = {
   status: 'LIVE',
@@ -28,7 +28,7 @@ const project4 = {
   title: 'Inktradex • Landing Page',
   body: 'Inktradex offers 360-degree translation solutions for companies of all sizes and sectors.',
   stack: 'Type-Script, Nextjs, Reactjs, styled-components, ',
-  svgPath: './src/assets/Inktradex.svg' 
+  svgPath: '/assets/Inktradex.svg' 
 };
 
 export const projects = [project1, project2, project3, project4];
